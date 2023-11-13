ef62521f2e5e9a9e27545fe50712d4bc7ea1fa6e
Parsa <parsasarfarazi@gmail.com>
2023-11-13 22:41:22 +0330

    Update Dapsi.sol

--- contracts/Dapsi.sol
+++ contracts/Dapsi.sol
@@ -1,54 +1,94 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
+// designed and developed by : Ali
+
 
 contract Dapsi {
-    mapping (address => uint) public availableRequests;
 
+    //client stuff
     struct Client  {
         address clientAddress;
         string clientName;
         bool hasRequest;
     }
+
     Client[] private clients;
-
+    mapping (address => uint) clientIndex;
     mapping (address => bool) isClient;
+    //client stuff
 
+    //driver Stuff
     struct Driver {
         address driverAddress;
         string driverName;
         bool inRequest;
     }
-
     Driver[] private drivers;
-
+    mapping (address => uint) driverIndex;
     mapping (address => bool) isDriver;
+    //driver stuff
 
-    modifier onlyClient() {
-        require(isClient[msg.sender] == true, "not a client");
-        _;
+    modifier clientModifier(bool hi) {
+        if (hi) {
+            require(isClient[msg.sender] == true, "not a client");
+            require(clients[clientIndex[msg.sender]].hasRequest == false, "you already have a request");
+            _;
+        }
+        else {
+            require(isClient[msg.sender] == false, "you are already a client");
+            _;
+        }
     }
 
-    modifier onlyDriver() {
-        require(isDriver[msg.sender] == true, "not a driver");
-        _;
+    modifier driverModifier(bool yo) {
+       if (yo) {
+            require(isDriver[msg.sender] == true, "not a driver");
+            require(drivers[driverIndex[msg.sender]].inRequest == false, "you are already in request");
+
+            _;         
+       }
+       else {
+            require(isDriver[msg.sender] == false, "you are already a driver");
+            _;
+       }
     }
 
-    function singUp(string memory _name) public {
+    // request stuff
+    struct Request {
+        address clientAddress;
+        address driverAddress;
+        string mabda;
+        string maghsad;
+        uint256 price;
+    }
+    Request[] public requests;
+    //request stuff
+
+    function signUpClient(string memory _name) public clientModifier(false) {
         clients.push(Client(msg.sender, _name, false));
         isClient[msg.sender] = true;
+        clientIndex[msg.sender] = clients.length;
+    }
+
+    function signUpDriver(string memory _name) public driverModifier(false){
+        clients.push(Client(msg.sender, _name, false));
+        isClient[msg.sender] = true;
+        driverIndex[msg.sender] = drivers.length;
     }
 
 
-   function requestTravel(string memory mabda, string memory maghsad) public onlyClient payable   {
+   function requestTravel(string memory mabda, string memory maghsad) public clientModifier(true) {
+
+        requests.push(Request(msg.sender, msg.sender ,mabda, maghsad, 10));
+        clients[clientIndex[msg.sender]].hasRequest = true;
         
    }
    
-   function generateBid()  public  {
-    
-   }
+   //function generateBid()  private   {
+   //}
 
-   function acceptRequest(uint256 _requestId) public onlyDriver() {
-    
+   function acceptRequest(uint256 _requestId) public driverModifier(true) {
+
    }
   
 }
